0ed78e52b126c89fa86adc1c9ab9a8f0e023d585
zs-barnes <zsbarnes@dons.usfca.edu>
2020-06-24 20:21:20 -0700

    update Sepsisenv and main.py

--- env/SepsisEnv.py
+++ env/SepsisEnv.py
@@ -10,28 +10,33 @@ MAX_OBSERVATION = 335.0
 class SepsisEnv(gym.Env):
     """A Sepsis environment for OpenAI gym"""
     metadata = {'render.modes': ['human']}
+    
     def __init__(self, df):
         super(SepsisEnv, self).__init__()
 
         self.df = df
         self.reward_range = (-2.0, 1.0)
+        # index of where each patient begins
+        self.patient_start_index = list(df.loc[df['index']==0].index)
+        self.index = 0
+        
         # Only two possible actions, 0 for non-sepsis,
-        # 1 for sepsis.
+        # 1 for sepsis
         n_actions = 2
         self.action_space = spaces.Discrete(n=n_actions)
+        
         # Observation space is a feature vector of 41 vital signs, 
-        # lab values, and other demographic information.
+        # lab values, and other demographic information
         self.observation_space = spaces.Box(
-            low=MIN_OBSERVATION, high=MAX_OBSERVATION, shape=(1, 42), dtype=np.float16)
-
+            low=-25.545204389483803, high=335.0, shape=(1, 42), dtype=np.float16)
 
     def _next_observation(self):
-        obs = np.array([self.df.loc[self.current_step,
-                        ~self.df.columns.isin(['SepsisLabel',
+        obs = np.array([
+        self.df.loc[self.current_step, ~self.df.columns.isin(['SepsisLabel',
                             'patient', 
                             'zeros_reward',
-                            'ones_reward'])].values 
-
+                            'ones_reward',
+                            'end_episode'])].values 
         ])
 
         return obs
@@ -40,29 +45,39 @@ class SepsisEnv(gym.Env):
     def step(self, action):
         # Execute one time step within the environment
         self.current_step += 1
-        
+#         print('current step', self.current_step)
+#         print('index', self.index)
+
+        done = False
         if action == 0:
             reward = self.df.loc[self.current_step, ['zeros_reward']]
         else:
             reward = self.df.loc[self.current_step, ['ones_reward']]
-
-        done = False
+        
+        end_episode = self.df.loc[self.current_step, ['end_episode']]
+        if end_episode.values == 1:
+            done = True
+            self.index += 1
 
         obs = self._next_observation()
 
         return obs, reward, done, {}
 
+    
     def reset(self):
         # Reset the state of the environment to an initial state
-
         # Set the current step to a random point within the data frame
-        self.current_step = random.randint(
-            0, len(self.df.loc[:, 'HR'].values))
+
+#         self.current_step = np.random.choice(patient_start_index)
+        self.current_step = self.patient_start_index[self.index]
+        
+
 
         return self._next_observation()
 
     def render(self, mode='human', close=False):
         # Render the environment to the screen
-        print('current step' , self.current_step)
+        print('current step' ,self.current_step)
+        print('index', self.index)  
 
 
--- main.py
+++ main.py
@@ -8,22 +8,47 @@ from stable_baselines import PPO2
 
 from env.SepsisEnv import SepsisEnv
 from load_data import load_data
-from add_reward import add_reward_df
-
+from add_reward import add_reward_df, add_end_episode_df
 import pandas as pd
+from tqdm import tqdm
+
+
+def train_model(df, env, model, total_timesteps, ):
+    model = PPO2(MlpPolicy, env, verbose=0)
+    model.learn(total_timesteps=20)
+    reward_list = []
+    obs = env.reset()
+    patient_count = 0
+    for _ in tqdm(range(200)):
+        action, _states = model.predict(obs)
+        obs, rewards, done, info = env.step(action)
+        reward_list.append(rewards)
+        if done:
+            patient_count += 1
+            obs = env.reset()
+        env.render()
+
+
 
 df = load_data()
 df = add_reward_df(df)
+df = add_end_episode_df(df)
 df = df.reset_index()
 
-# The algorithms require a vectorized environment to run
 env = DummyVecEnv([lambda: SepsisEnv(df)])
-
-model = PPO2(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=20000)
-
+model = PPO2(MlpPolicy, env, verbose=0)
+model.learn(total_timesteps=20)
+reward_list = []
 obs = env.reset()
-for i in range(2000):
+patient_count = 0
+for _ in tqdm(range(200)):
     action, _states = model.predict(obs)
     obs, rewards, done, info = env.step(action)
+    reward_list.append(rewards)
+    if done:
+        patient_count += 1
+        obs = env.reset()
     env.render()
+
+print('Total patients: ', patient_count)
+print('Total reward:', sum(reward_list))
